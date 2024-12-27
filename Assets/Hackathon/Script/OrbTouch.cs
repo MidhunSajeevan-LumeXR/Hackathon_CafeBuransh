ceafb3dc7cf81fe39d49c2859e30fbe09a5b16ce
Midhun Sajeevan <midhun.sajeevan@terrificminds.com>
2024-12-27 18:55:00 +0530

    lotus flower updated

--- Assets/Hackathon/Script/OrbTouch.cs
+++ Assets/Hackathon/Script/OrbTouch.cs
@@ -1,13 +1,19 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class OrbTouch : MonoBehaviour
 {
     [SerializeField] private OrbContents orbContent;
+    [SerializeField] private GameObject controllerOrb;
     public UnityEvent OnTriggerEntered;
+    public TextMeshProUGUI text;
 
-    public void OnTriggerEnter()
+    public void OnTriggerEnter(Collider other)
     {
+        controllerOrb.transform.position = this.transform.position;
+        controllerOrb.SetActive(true);
+        text.text = other.name.ToString();
         Debug.Log("Trigger Entered");
         OnTriggerEntered?.Invoke();
         UIEventManager.instance.SetData(orbContent);
